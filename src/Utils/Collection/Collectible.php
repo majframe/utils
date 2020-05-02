fd5b3e48f92b179782d7c55f46059b0686b42bef
Ondřej Maxa <ondrej.maxa.ih1@gmail.com>
2020-05-02 20:57:19 +0200

    ArrayList.php

---
+++ src/Utils/Collection/Collectible.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * 2020-2020 Majframe
+ *
+ *  NOTICE OF LICENSE
+ *
+ * This source file is subject to the GNU General Public License (GPL 3.0)
+ * that is bundled with this package in the file LICENSE.md.
+ * It is also available through the world-wide-web at this URL:
+ * https://www.gnu.org/licenses/gpl-3.0.html
+ *
+ * @copyright 2020-2020 Majframe
+ * @license   https://www.gnu.org/licenses/gpl-3.0.html GNU General Public License (GPL 3.0)
+ */
+
+namespace Majframe\Utils\Collection;
+
+
+interface Collectible
+{
+    public function add($value): void;
+    public function get($index);
+    public function set($index, $value);
+    public function remove($index): void;
+    public function exists($index): bool;
+    public function asArray(): array;
+}
